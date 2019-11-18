772c6a32c0892eeaa347f02ce9a922568f07ed3e
Rob McMullen <feedback@playermissile.com>
2019-11-18 10:03:24 -0800

    Added logic to move actual players and changed when players are moved to groups of 3

--- vcount_multi_player/vcount_multi_player.s
+++ vcount_multi_player/vcount_multi_player.s
@@ -88,17 +88,17 @@ mindelay = 3 * gunstepinit
 gundata = $2e00
 
 ; actual players are on the top level, copies are below
-vcount1 = 21
-vcount2 = 38
-vcount3 = 55
+vcount1 = 24
+vcount2 = 40
+vcount3 = 56
 vcount4 = 72
 vcount5 = 88
 vcount6 = 104
 
 ; for 6 pixel height
 roomba_height = roomba2 - roomba1
-band1 = (2*vcount2) - 8
-band2 = (2*vcount3) - 10
+band1 = (2*vcount2) - 12
+band2 = (2*vcount3) - 12
 band3 = (2*vcount4) - 12
 band4 = (2*vcount5) - 12
 band5 = (2*vcount6) - 12
@@ -119,56 +119,59 @@ gameloop
         cmp #$02        ; dead with birdies?
         beq ?glcont
 
+        ; roombamoveindex will get set to zero at the vertical blank which will
+        ; trigger the player moving logic. One player is moved every time
+        ; through the game loop, and because moving a single player only takes
+        ; a few scan lines we will stay ahead of the vcount bands.
+        ldx roombamoveindex
+        cpx #(roombadx - roombax) ; number of roombas
+        bcs ?glvcount   ; skip movement until next VBI
+
+        jsr moveplayer
+        inc roombamoveindex
+
+?glvcount
         lda vcount
 
         ; check if vcount is in layer 1 (between vcount1 and vcount2)
         cmp #vcount1
         bne ?v2
 
-        ; do some housekeeping the first time through every frame
-        lda #10
-        sta colbak
-        inc roombacounter
-
         ; set player location for band 1
-        ldx #0
-        jsr moveplayers
+        ldx #2
+        jsr show_players_in_band
         jmp ?glcont
 
         ; check in layer 2 (between vcount2 and vcount3)
 ?v2     cmp #vcount2
         bne ?v3
         ; set player location for band 2
-        ldx #2
-        stx colbak
-        jsr moveplayers
+        ldx #4
+        jsr show_players_in_band
         jmp ?glcont
 
         ; check in layer 3 (between vcount3 and vcount4)
 ?v3     cmp #vcount3
         bne ?v4
         ; set player location for band 3
-        ldx #4
-        stx colbak
-        jsr moveplayers
+        ldx #6
+        jsr show_players_in_band
         jmp ?glcont
 
         ; check in layer 4 (between vcount4 and vcount5)
 ?v4     cmp #vcount4
         bne ?v5
         ; set player location for band 4
-        ldx #6
-        stx colbak
-        jsr moveplayers
+        ldx #8
+        jsr show_players_in_band
         jmp ?glcont
 
         ; check in layer 5 (between vcount5 and vcount6)
 ?v5     cmp #vcount5
         bne ?glcont
         ; set player location for band 5
-        ldx #8
-        stx colbak
-        jsr moveplayers
+        ldx #10
+        jsr show_players_in_band
 
 ?glcont lda jmstatus
 ?gl2    cmp #$08
@@ -189,18 +192,20 @@ playerinit
         lda #(roomba2 - roomba1)
         sta p2height
         sta p3height
-        lda #56
+        lda roombax
         sta p2x
-        lda #180
+        lda roombax+1
         sta p3x
-        lda #36
+        lda roombay
         sta p2y
+        lda roombay+1
         sta p3y
         lda #1
         sta p2active
         sta p3active
-        sta p3frame
-        lda #2
+        lda roombaframe
+        sta p2frame
+        lda roombaframe+1
         sta p2frame
         ;jsr testplayers_vertical
         lda #<roomba1
@@ -209,32 +214,25 @@ playerinit
         sta src+1
         ldx #9
 ?copyloop
-        jsr copy_player_to_band
+        jsr fast_copy_player_to_band
         dex
         bpl ?copyloop
         rts
 
-; only move half the players every frame so it doesn't clog up the cycles
-; and cause the next vcount test to be missed
-moveplayers
-        lda roombacounter
-        and #1
-        bne ?p3
-        jsr moveplayer ; move p2, leave p3 where it is
+show_players_in_band
+        stx colbak
         lda roombax,x
         sta hposp2
         inx
         lda roombax,x
         sta hposp3
         rts
-?p3     lda roombax,x ; move p3, leave p2 where it is
-        sta hposp2
-        inx
-        jsr moveplayer
-        lda roombax,x
-        sta hposp3
-        rts
+
+; move a third of the players every frame to reduce speed of roombas
 moveplayer ; player number in x
+        lda roombagroupindex ; only move when counter matches
+        cmp roombagroup,x
+        bne ?exit
         clc
         lda roombax,x
         adc roombadx,x
@@ -255,14 +253,30 @@ moveplayer ; player number in x
         lda #1
         sta roombaframe,x
 ?done   
-;        rts
+        cpx #2          ; first 2 players are the actual players
+        bcs copy_player_to_band
+        lda roombax,x
+        tay
+        lda roombaframe,x
+        cpx #1
+        beq ?change_real_p3
+?change_real_p2
+        sta p2frame
+        sty p2x
+        rts
+?change_real_p3
+        sta p3frame
+        sty p3x
+?exit
+        rts
 
+copy_player_to_band
         lda #<roomba1
         sta src
         lda #>roomba1
         sta src+1
 
-copy_player_to_band
+fast_copy_player_to_band
         lda roombadest,x
         sta ?loop_smc+2
         lda roombay,x
@@ -316,21 +330,32 @@ move    lda p2x         ; restore original players for top of next frame
         sta hposp2
         lda p3x
         sta hposp3
+
+        lda #0
+        sta roombamoveindex ; start next frame with first roomba
+        inc roombagroupindex ; check and reset roomba group to 0, 1, or 2
+        lda roombagroupindex
+        cmp #3
+        bcc ?1
+        lda #0
+        sta roombagroupindex
 ;        lda ls_player2_color
 ;        sta colpm2
 ;        lda ls_player3_color
 ;        sta colpm3
-        jmp $311b
+?1      jmp $311b
 
 roomba1 .byte $3c, $7e, $ff, $ff, $ff, $55
 roomba2 .byte $3c, $7e, $ff, $ff, $ff, $aa
-roombacounter .byte 0
+roombagroupindex .byte 0
+roombamoveindex .byte 0
 
-; band data, 2 roombas per band
-roombax .byte 100, 120, 80, 140, 60, 160, 70, 170, 60, 180
-roombadx .byte 1, $ff, 1, $ff, 1, $ff, 1, $ff, 1, $ff
-roombaminx .byte 60, 138, 60, 138, 60, 138, 60, 138, 60, 138,
-roombamaxx .byte 115, 190, 115, 190, 115, 190, 115, 190, 115, 190,
-roombay .byte band1, band1, band2, band2, band3, band3, band4, band4, band5, band5
-roombaframe .byte 2, 1, 1, 2, 2, 1, 1, 2, 2, 1
-roombadest .byte $66, $67, $66, $67, $66, $67, $66, $67, $66, $67
+; band data, 2 roombas per band, 3 groups
+roombax .byte 60, 180, 100, 120, 80, 146, 60, 160, 70, 170, 60, 180
+roombadx .byte 1, $ff, 1, $ff, 1, $ff, 1, $ff, 1, $ff, 1, $ff
+roombaminx .byte 60, 138, 60, 138, 60, 146, 60, 138, 60, 138, 60, 138,
+roombamaxx .byte 115, 190, 115, 190, 100, 190, 111, 190, 115, 190, 115, 190,
+roombay .byte 36, 36, band1, band1, band2, band2, band3, band3, band4, band4, band5, band5
+roombaframe .byte 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1
+roombagroup .byte 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2
+roombadest .byte $ff, $ff, $66, $67, $66, $67, $66, $67, $66, $67, $66, $67
